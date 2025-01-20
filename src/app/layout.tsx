517a22f99026e008fac4956773138c89dd2bd72a
Filipe Prado <oi.fprado@gmail.com>
2025-01-20 10:57:30 -0300

    chore: solving lint errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {
-	ClerkProvider,
-	SignedIn,
-	SignedOut,
-	UserButton,
-	SignInButton,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
--- src/components/service-form.tsx
+++ src/components/service-form.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import React from "react";
-import { useForm, Controller } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
 	Form,
 	FormControl,
@@ -64,7 +63,7 @@ export default function ServiceForm() {
 	const formValues = watch();
 
 	const onSubmit = (data: FormValues) => {
-		console.log(data);
+		console.log(data, formValues);
 		// Handle form submission here
 	};
 
